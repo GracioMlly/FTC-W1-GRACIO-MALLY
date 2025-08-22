75d3847118b1e1920d03107af1f9155174d3dcd7
Gracio <graciomlly@gmail.com>
2025-08-22 19:14:46 +0000

    feat: add mobile navigation opening and closing transition

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import "./css/reset.css"
-import "./css/style.css"
+import "./css/reset.css";
+import "./css/style.css";
 
-console.log("Hello world !");
+const openMenuButton = document.getElementById("openMenuButton");
+const closeMenuButton = document.getElementById("closeMenuButton");
+const mobileNav = document.querySelector(".navbar__menu-mobile");
+
+openMenuButton.addEventListener("click", () => {
+  mobileNav.classList.toggle("openingMenu");
+});
+
+closeMenuButton.addEventListener("click", () => {
+  mobileNav.classList.toggle("openingMenu");
+});
